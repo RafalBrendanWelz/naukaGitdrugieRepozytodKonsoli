040958b5d2cbe61031804728a12b4840f3570ae1
RafalBrendanWelz <wadWESDSWS@O2.PL>
2019-11-16 13:48:50 +0100

    Beta 0.2 dodanie gitignore i drobny komentarz w kodzie

--- palindrome/src/com/company/Main.java
+++ palindrome/src/com/company/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Witamy w programie z palindromami");
+        //terefere
 
-
-
-	// write your code here
     }
 }
